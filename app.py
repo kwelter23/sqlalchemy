57f0d4f964029a15ce1d2afa3eafda8aec4c2a71
Welter <kwelter@916schools.org>
2020-08-21 21:10:26 -0500

    start date f loop

--- app.py
+++ app.py
@@ -36,7 +36,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/start<start><br/>"
+        f"/api/v1.0/start/enddate<start>/<end>"
     )
 
 #Precipitation Dictionary
@@ -46,19 +47,18 @@ def precipitation():
     session = Session(engine)
 
      # Query precipitation
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    results = session.query(Measurement.date, Measurement.prcp).filter(func.strftime(Measurement.date) > '2016-08-23').all()
 
     session.close
 
     # Create a dictionary from the row data and append to a list 
-    precipitation_list = []
+    prcp_list = [] 
     for date, prcp in results:
-        precipitation_dict = {}
-        precipitation_dict["date"] = date
-        precipitation_dict["prcp"] = prcp
-        precipitation_list.append(precipitation_dict)
+        prcp_dict = {}
+        prcp_dict[date] = prcp
+        prcp_list.append(prcp_dict)
 
-    return jsonify(precipitation_list)
+    return jsonify(prcp_list)
 
 
 #All Stations
@@ -67,9 +67,9 @@ def stations():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
+    """Return a list of all stations"""
     # Query all passengers
-    results = session.query(Measurement.station).all()
+    results = session.query(Measurement.station).filter(func.strftime(Measurement.date) > '2016-08-23').all()
 
     session.close()
 
@@ -101,14 +101,34 @@ def tobs():
     return jsonify(most_observed_temps)
 
 #Minimum Temperature, average temperature and max temperature for a date range
+#Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+@app.route("/api/v1.0/start/<start>")
+def temp_stats_with_start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query precipitation
+    results = session.query(Measurement.date, Measurement.tobs).all()
+
+    session.close
+
+    temp_list = [] 
+    for date, tobs in results:
+        #search_term = date
+        #temp_dict = {}
+        #temp_dict = tobs
+
+        if date >= start:
+            temp_list.append(tobs)
+
+    return jsonify(temp_list)           
+ 
 
 
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive.
+#@app.route("/api/v1.0/<start>/<end>")
+#def startendtemp():
 
 
 
